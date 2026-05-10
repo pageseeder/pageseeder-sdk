158073d0f58ccbbc04bf7bf1bdd56cfdad7b2ce4
clauret <clauret@weborganic.com>
2026-05-11 09:18:08 +1000

    Chore: Update `VersionCommand` to use `get(0)` instead of `getFirst()` for argument retrieval

--- sdk-cli/src/main/java/org/pageseeder/sdk/cli/VersionCommand.java
+++ sdk-cli/src/main/java/org/pageseeder/sdk/cli/VersionCommand.java
@@ -38,7 +38,7 @@ final class VersionCommand implements CliCommand {
     }
 
     try {
-      URI apiOrigin = URI.create(arguments.getFirst());
+      URI apiOrigin = URI.create(arguments.get(0));
       PageSeederClient client = PageSeederClient.builder()
           .apiOrigin(apiOrigin)
           .build();
