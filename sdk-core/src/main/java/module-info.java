45113312d724c3516b7f9714d442f3938c75420e
clauret <clauret@weborganic.com>
2026-05-10 23:41:07 +1000

    Chore: Add module-info.java files for SDK core and model modules to define module boundaries

---
+++ sdk-core/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.pageseeder.sdk.core {
+    requires java.net.http;
+    requires java.xml;
+    requires org.slf4j;
+    requires static org.jspecify;
+
+    exports org.pageseeder.sdk;
+    exports org.pageseeder.sdk.auth;
+    exports org.pageseeder.sdk.client;
+    exports org.pageseeder.sdk.exception;
+    exports org.pageseeder.sdk.oauth;
+    exports org.pageseeder.sdk.service;
+    exports org.pageseeder.sdk.xml.sax;
+    exports org.pageseeder.sdk.xml.stax;
+}
---
+++ sdk-model/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.pageseeder.sdk.model {
+    requires transitive org.pageseeder.sdk.core;
+    requires transitive com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.xml;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires java.xml;
+    requires static org.jspecify;
+
+    exports org.pageseeder.sdk.model;
+    exports org.pageseeder.sdk.model.codec;
+}
